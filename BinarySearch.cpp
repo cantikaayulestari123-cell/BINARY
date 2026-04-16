b246359de1243219926a66ed1226ada7e309ff2e
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-04-16 10:16:34 +0700

    menambahkan library

---
+++ BinarySearch.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int element[10];
+int panjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+    }
+}
