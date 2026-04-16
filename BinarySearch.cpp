d0e4535d59b32ec3154314159f7000c1b49abedf
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-04-16 10:28:23 +0700

    menampilkan banyaknya elemen pada array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int element[10]; 
-int panjang;
+int nPanjang;
 int x;
 
 void input() 
@@ -10,5 +10,20 @@ void input()
     while (true)
     {
         cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
-    } 
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+        break;
+        }
+        else
+        {
+            cout << "\n===========================\n";
+            cout << "       Masukkan Elemen Array   ";
+            cout << "=============================\n";
+
+            
+        }
+
+    }
 }
